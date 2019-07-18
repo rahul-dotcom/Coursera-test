f0486e40441b4b40e1061338357d7baaf3e4d128
rahul-dotcom <kanhaiyajee1997@gmail.com>
2019-07-18 11:25:45 +0530

    first file .

---
+++ app.js
@@ -0,0 +1,25 @@
+var student ={
+name:"", type: "student"
+};
+document.addEventListener('DOMContentLoaded',contentLoaded);
+
+function contentLoaded(event) {
+  document.getElementById('name').addEventListener("keyup",keyup);
+}
+
+function keyUp(event) {
+  calculateNumericOutput();
+}
+
+function calculateNumericOutput() {
+  student.name=document.getElementById('name').value;
+
+  var totalNameValue =0;
+  for(var i=0;i<student.name.length;i++){
+    totalNameValue += student.name.charCodeAt(i);
+  }
+
+  //insert result into page
+  var output ="Total numeric value of persion's name is " + totalNameValue;
+  document.getElementById('output').innerText =output;
+}
